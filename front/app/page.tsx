e3f1a832f301cd1d6f57e32949c5004ebbeee30b
FallenSoul <asimboss00789@gmail.com>
2026-05-26 02:22:28 +0300

    Update page.tsx

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,5 +1,5 @@
-import GameCard from '@/components/GameCard'
-import Navbar from '@/components/Navbar'
+import GameCard from '../components/GameCard'
+import Navbar from '../components/Navbar'
 import { ExternalLink, Github, Twitter } from 'lucide-react'
 import Link from 'next/link'
 import { GameInfo } from '../types'
@@ -49,21 +49,21 @@ export default async function Home() {
       <div className="grid grid-cols-1 gap-4 md:grid-cols-3 my-4">
         <Link
           href={'https://discord.gg/kPhgtj49U2'}
-          className="flex py-2 items-center justify-center   px-8   font-medium   border border-transparent rounded-md hover:bg-gray-100   md:text-lg md:px-10"
+          className="flex py-2 items-center justify-center px-8 font-medium border border-transparent rounded-md hover:bg-gray-100 md:text-lg md:px-10"
         >
           <ExternalLink className="mr-2" />
           Project Discord
         </Link>
         <Link
           href={'https://twitter.com/iErcan_'}
-          className="flex py-2 items-center justify-center  px-8   font-medium   border border-transparent rounded-md hover:bg-gray-100    md:text-lg md:px-10"
+          className="flex py-2 items-center justify-center px-8 font-medium border border-transparent rounded-md hover:bg-gray-100 md:text-lg md:px-10"
         >
           <Twitter className="mr-2" />
           Twitter
         </Link>
         <Link
           href={'https://github.com/iErcann/Notblox'}
-          className="flex py-2 items-center justify-center   px-8   font-medium  border border-transparent rounded-md hover:bg-gray-100   md:text-lg md:px-10"
+          className="flex py-2 items-center justify-center px-8 font-medium border border-transparent rounded-md hover:bg-gray-100 md:text-lg md:px-10"
         >
           <Github className="mr-2" />
           Source Code
