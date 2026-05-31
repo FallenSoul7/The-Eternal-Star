bbff3d450a59f79566c802f44f9e9b525da84a2a
FallenSoul <asimboss00789@gmail.com>
2026-05-31 22:21:20 +0300

    Update Navbar.tsx

--- front/components/Navbar.tsx
+++ front/components/Navbar.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+
 export default function Navbar() {
   return (
     <div className="flex items-center justify-between px-4 py-3 border-b border-slate-800">
       <span className="text-white font-black text-lg tracking-tight">The Eternal Star</span>
       <div className="flex items-center gap-3">
       </div>
+    </div> // <-- This is the missing closing tag
   )
 }
