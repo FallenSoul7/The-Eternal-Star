440b687b64831423206d731fc751a3b5bd1fe068
FallenSoul <asimboss00789@gmail.com>
2026-06-09 23:33:44 +0300

    Update types.ts

--- front/types.ts
+++ front/types.ts
@@ -1,9 +1,9 @@
 export interface GameInfo {
-  id: string | number // FIXED: Accepts both string and number IDs
+  id: string | number
   title: string
   slug: string
   imageUrl: string
-  websocketPort: number
+  websocketPort?: number // Optional now!
   images?: { 
     url: string; 
     width: number; 
